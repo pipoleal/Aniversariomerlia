a3b87b43c5275c842c3beaa0e1319d2e49a26ebc
felipe leal <felipelealdev@gmail.com>
2026-02-09 21:27:36 -0300

    commit

--- js/confirmacao.js
+++ js/confirmacao.js
@@ -1,27 +1,32 @@
-const form = document.getElementById("form-confirmacao");
-const status = document.getElementById("mensagem-status");
+var form = document.getElementById("form-confirmacao");
+var status = document.getElementById("mensagem-status");
 
-const URL_BACKEND = "https://script.google.com/macros/s/AKfycbzDbEraQjDQ9MtU3cHC-lS5NdzhMqIOTPJeIfMtNuMuZNi53ywBjTeL7IcMHy1dcK-D5g/exec";
+var URL_BACKEND =
+  "https://script.google.com/macros/s/AKfycbwTzSoFM3aVBm3LsFrEuBQjSG7If9ZP0pVLKF0ZHiJTwH6pw8SFCISlrOBhvjE93S7l/exechttps://script.google.com/macros/s/AKfycbzDbEraQjDQ9MtU3cHC-lS5NdzhMqIOTPJeIfMtNuMuZNi53ywBjTeL7IcMHy1dcK-D5g/exec";
 
-form.addEventListener("submit", async (e) => {
+form.addEventListener("submit", function (e) {
   e.preventDefault();
 
-  const nome = document.getElementById("nome").value;
-  const presenca = document.getElementById("presenca").value;
+  var nome = document.getElementById("nome").value;
+  var presenca = document.getElementById("presenca").value;
 
   if (!nome || !presenca) return;
 
   status.textContent = "Enviando confirmação...";
 
-  try {
-    await fetch(URL_BACKEND, {
-      method: "POST",
-      body: JSON.stringify({ nome, presenca }),
-    });
+  var dados = new FormData();
+  dados.append("nome", nome);
+  dados.append("presenca", presenca);
 
-    status.textContent = "Confirmação enviada com sucesso 💖";
-    form.reset();
-  } catch {
-    status.textContent = "Erro ao enviar confirmação 😢";
-  }
+  fetch(URL_BACKEND, {
+    method: "POST",
+    body: dados
+  })
+    .then(function () {
+      status.textContent = "Confirmação enviada com sucesso 💖";
+      form.reset();
+    })
+    .catch(function () {
+      status.textContent = "Erro ao enviar confirmação 😢";
+    });
 });
